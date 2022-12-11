bcedd127ed9a2eda7563f7f5093903822b2b36c5
HoshimiAkira <3172237114@qq.com>
2022-12-11 09:11:39 +0800

    subtitle

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from pymongo import MongoClient
 import jwt
 import bcrypt
-import datetime
+from datetime import date
 from bson import ObjectId
 from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient,BlobBlock
@@ -135,7 +135,7 @@ def upload():
         videos.insert_one(info)
         return make_response(jsonify( {'success':"Upload success."}), 200)
     except:
-            return make_response(jsonify( {'message':"Upload fail.Please Change your file name"}), 400)
+        return make_response(jsonify( {'message':"Upload fail.Please Change your file name"}), 400)
 
 @app.route("/api/v1.0/video", methods=["GET"])
 def show_all_videos():
@@ -166,7 +166,10 @@ def show_all_videos():
         data={
             "id":video["_id"],
             "picSrc":video["cover"],
-            "title":video["title"]
+            "title":video["title"],
+            "views":video["views"],
+            "collect":video["collect"],
+            "date":video["date"]
         }
         
         data_to_return.append(data)
@@ -177,25 +180,67 @@ def show_all_videos():
 @app.route("/api/v1.0/video/<string:id>", methods=["GET"])
 def show_one_video(id):
     data_to_return=[]
+    comment_to_return=[]
+    page_num, page_size = 1, 10
+    if request.args.get('pn'):
+        page_num = int(request.args.get('pn'))
+    if request.args.get('ps'):
+        page_size = int(request.args.get('ps'))
+    page_start = (page_size * (page_num - 1))
     video = videos.find_one({'_id':ObjectId(id)})
     if video is not None:
+        videos.update_one( { "_id" : ObjectId(id) }, { "$set": { "views" : video["views"]+1 } } )
         video['_id'] = str(video['_id'])
+        video["views"]+=1
+        for comment in video["comment"][page_start:page_start+page_size]:
+            comment["_id"]=str(comment["_id"])
+            comment_to_return.append(comment)
+        video["comment"]=comment_to_return
         data_to_return.append(video)
         return make_response( jsonify( data_to_return ), 200 )
     else:
-        return make_response( jsonify( {"error" : "Invalid video ID"} ), 404 )
+        return make_response( jsonify( {"message" : "Invalid video ID"} ), 404 )
     
 @app.route("/api/v1.0/video/<string:id>/comment", methods=["POST"])
 def add_comment(id):
-    new_comment = {
-        "_id" : ObjectId(),
-        "user_id" : request.form["user_id"],
-        "comment" : request.form["comment"],
-        "mark" : request.form["mark"]
-    }
-    videos.update_one( { "_id" : ObjectId(id) }, { "$push": { "comment" : new_comment } } )
-    return make_response( jsonify( { "message" : "Add Comment Success" } ), 201 )
+    video = videos.find_one({'_id':ObjectId(id)})
+    if video is not None:
+        new_comment = {
+            "_id" : ObjectId(),
+            "user_id" : request.form["user_id"],
+            "username" : request.form["username"],
+            "comment" : request.form["comment"],
+            "mark" : request.form["mark"],
+        }
+        print(new_comment["user_id"])
+        print(new_comment["comment"])
+        print(new_comment["mark"])
+        videos.update_one( { "_id" : ObjectId(id) }, { "$push": { "comment" : new_comment } } )
+        return make_response( jsonify( { "success" : "Add Comment Success" } ), 201 )
+    else:
+        return make_response( jsonify( {"message" : "Invalid video ID"} ), 404 )
 
+@app.route("/api/v1.0/video/<string:id>/subtitle", methods=["POST"])
+def add_subtitle(id):
+    video = videos.find_one({'_id':ObjectId(id)})
+    if video is not None:
+        files=request.files
+        tempFilePath = tempfile.gettempdir()
+        subtitle=files["subtitle"]
+        subtitleName=subtitle.filename
+        subtitle.save(tempFilePath+"/"+subtitleName)
+        subtitle_path = os.path.join(tempFilePath, subtitleName)
+        subtitle_client = blob_service_client.get_blob_client(container="havideoassvtt", blob=subtitleName)
+        try:
+            with open(file=subtitle_path, mode="rb") as vttdata:
+                subtitle_client.upload_blob(vttdata)
+            vtturl="https://havideoassblob.blob.core.windows.net/havideoassvtt/"+subtitleName
+            videos.update_one( { "_id" : ObjectId(id) }, { "$set": { "vtt" : vtturl } } )
+            return make_response( jsonify( { "success" : "Add subtitle Success" } ), 201 )
+        except:
+            return make_response(jsonify( {'message':"Upload fail.Please Change your file name"}), 400)
+    else:
+        return make_response( jsonify( {"message" : "Invalid video ID"} ), 404 )
 
 
 
@@ -254,7 +299,12 @@ def testregister():
     return make_response(jsonify( {'token':"success"}), 200)
 @app.route('/api/v1.0/fix', methods=['GET'])
 def fix(): 
-    videos.update_many({},{"$set":{"producer":"Unknown Producer"}},upsert=True)
+    fix=date.today().isoformat()
+    videos.update_many({},{"$set":{"date":fix}},upsert=True)
+    for video in videos.find().sort("_id",-1):
+        for comment in video["comment"]:
+            comment["date"]=fix
+        videos.update_one({"_id" : video["_id"]},{"$set":{"comment":video["comment"]}},upsert=True)
     return make_response(jsonify( {'token':"success"}), 200)
 
     
